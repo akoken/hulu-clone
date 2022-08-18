be2d222f941d68ba34ab529cefd5ee0757e9ac31
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-08-18 22:52:53 +0300

    Update route names

--- utils/requests.js
+++ utils/requests.js
@@ -1,47 +1,47 @@
 const API_KEY = process.env.API_KEY;
 
 export default {
-    fetchTrending:{
+    trending:{
         title: 'Trending',
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     },
-    fetchTopRated:{
+    topRated:{
         title: 'Top Rated',
         url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     },
-    fetchActionMovies:{
+    action:{
         title: 'Action',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     },
-    fetchComedyMovies:{
+    comedy:{
         title: 'Comedy',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     },
-    fetchHorrorMovies:{
+    horror:{
         title: 'Horror',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     },
-    fetchRomanceMovies:{
+    romance:{
         title: 'Romance',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     },
-    fetchMysteryMovies:{
+    mystery:{
         title: 'Mystery',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
     },
-    fetchSciFiMovies:{
+    scifi:{
         title: 'Science Fiction',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
     },
-    fetchWesternMovies:{
+    western:{
         title: 'Western',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
     },
-    fetchAnimationMovies:{
+    animation:{
         title: 'Animation',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
     },
-    fetchTVMovies:{
+    tvMovies:{
         title: 'TV Movie',
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
     },
